4549e13d75c2f44f299b7ea201c88e00cbc83965
Allaga <Allaga59@yandex.ru>
2021-03-25 07:37:59 +0300

    Добавлен код модального окна для кнопки Заказать звонок

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', () => {
+// modal
+	const modalUp = () => {
+		const modalCallback = document.querySelector('.modal-callback'), // окно
+			btn = document.querySelector('.callback-btn'),
+			modalOverlay = document.querySelector('.modal-overlay'),
+			modalClose = document.querySelector('.modal-close');
+		function binModal(trigger, modal, close) {
+			// открытие модального окна
+			trigger.addEventListener('click', e => {
+				// запрещаем стандартное событие. чтобы небыло перезагрузки
+				e.preventDefault();
+				modal.style.display = 'block';
+				modalOverlay.style.display = 'block';
+				document.body.style.overflow = 'hidden';
+			});
+			// закрытие окна
+			close.addEventListener('click', () => {
+				modal.style.display = 'none';
+				modalOverlay.style.display = 'none';
+				document.body.style.overflow = '';
+			});
+			// подложка
+			modal.addEventListener('click', e => {
+				if (e.target === modal) {
+					modal.style.display = 'none';
+					modalOverlay.style.display = 'none';
+				}
+			});
+		}
+		binModal(btn, modalCallback, modalClose);
+	};
+	modalUp();
+});
