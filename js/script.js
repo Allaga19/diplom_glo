6fd30d4ceb9fcad3bba7e50bf99850c57a9fda7d
Allaga <Allaga59@yandex.ru>
2021-03-26 14:38:09 +0300

    c

--- js/script.js
+++ js/script.js
@@ -33,3 +33,31 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 	modalUp();
 });
+// Кнопка вверх
+(function() {
+	const btnUp = document.querySelector('.up');
+	function trackScroll() {
+		const scrolled = window.pageYOffset;
+		let clientHeight = document.documentElement.clientHeight;
+		// const coords = clientHeight;
+		clientHeight = 30;
+		// условия для класса show
+		if (scrolled > clientHeight) {
+			btnUp.classList.add('up-show');
+		}
+		if (scrolled < clientHeight) {
+			btnUp.classList.remove('up-show');
+		}
+	}
+	// скорость прокрутки.
+	function backToTop() {
+		if (window.pageYOffset > 0) {
+			window.scrollBy(0, -80);
+			setTimeout(backToTop, 0);
+		}
+	}
+	// scroll отслеживать прокрутку документа(т.е. страницы)
+	window.addEventListener('scroll', trackScroll);
+	// click отслеживать нажатие на кнопку и приводить в действие скролл «наверх»
+	btnUp.addEventListener('click', backToTop);
+})();
