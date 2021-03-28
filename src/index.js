f939310df292947818dbeed279e3e3efdce75929
Allaga <Allaga59@yandex.ru>
2021-03-28 09:24:09 +0300

    Создана ветка ourServices. Внесены правки

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 
 import modalUp from './modules/modalUp';
 import scrollUp from './modules/scrollUp';
+import ourServices from './modules/ourServices';
 
 // modal
 modalUp();
 // кнопка скрола
 scrollUp();
+// наши услуги
+ourServices();
